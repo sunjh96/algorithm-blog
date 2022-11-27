218903aa972562f51c9486ce135e029d6611e1fa
sunjh96 <sunjh96@gmail.com>
2022-11-27 14:20:28 +0900

    프로젝트 생성

---
+++ src/main/java/com/devjeong/algorithmblog/AlgorithmBlogApplication.java
@@ -0,0 +1,13 @@
+package com.devjeong.algorithmblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgorithmBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjeong/algorithmblog/AlgorithmBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjeong.algorithmblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
