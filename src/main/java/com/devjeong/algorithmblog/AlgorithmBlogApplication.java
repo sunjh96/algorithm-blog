52cb4ca42ac025eda539e08c2509d04c0fc420ab
sunjh96 <sunjh96@gmail.com>
2022-11-28 10:48:03 +0900

    Feat : mysql 연동

--- src/main/java/com/devjeong/algorithmblog/AlgorithmBlogApplication.java
+++ src/main/java/com/devjeong/algorithmblog/AlgorithmBlogApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AlgorithmBlogApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlgorithmBlogApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmBlogApplication.class, args);
+    }
 
 }
--- src/main/java/com/devjeong/algorithmblog/Controller/HelloWorldController.java
+++ src/main/java/com/devjeong/algorithmblog/Controller/HelloWorldController.java
@@ -1,13 +1,19 @@
 package com.devjeong.algorithmblog.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+//@RestController
+@Controller
 public class HelloWorldController {
 
-    @GetMapping("/api/hello")
-    public String test() {
-        return "Hello, world!!";
+//    @GetMapping("/api/hello")
+//    public String test() {
+//        return "Hello, world!!";
+//    }
+
+    @GetMapping("/hi")
+    public String tst() {
+        return "/index.html";
     }
 }
