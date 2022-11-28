366fbc6bf9b2cb085e798e3639684e6f4c1331f3
sunjh96 <sunjh96@gmail.com>
2022-11-28 11:27:12 +0900

    Chore : Formatter 수정 1. Google -> Naver 수정

---
+++ src/main/java/com/devjeong/algorithmblog/home/model/UserModel.java
@@ -0,0 +1,33 @@
+package com.devjeong.algorithmblog.home.model;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity // User 클래스가 MySQL에 테이블 자동 생성
+public class UserModel {
+
+	@Id //Primary Key
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // 프로젝트에서 연결된 DB의 넘버링 전략을 따라간다.
+	private int id; //시퀀스, auto-increment
+
+	@Column(nullable = false, length = 30)
+	private String username; //아이디
+
+	@Column(nullable = false, length = 100) //해쉬 (비밀번호 암호화)
+	private String password;
+
+	@Column(nullable = false, length = 50)
+	private String email;
+
+	private String role; //Enum으로 변경 예정
+
+	@CreationTimestamp // 시간이 자동 입력
+	private Timestamp createDate;
+}
